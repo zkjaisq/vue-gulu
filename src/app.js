d25c1dc743a4f4fbb4b76a24acc200c6a80b67b4
zkjaisq <18771964684@163.com>
2019-03-06 14:43:22 +0800

    icon组件化

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Button from './button';
+import Icon from './icon.vue';
 Vue.component('g-button',Button);//注册全部的组件
+Vue.component('g-icon',Icon);//全局注册组件
 new Vue({
     el:'#app',
     data:{
---
+++ src/icon.vue
@@ -0,0 +1,13 @@
+<template>
+    <svg class="icon">
+        <use :xlink:href=`#i${name}`></use>
+    </svg>
+</template>
+<style>
+
+</style>
+<script>
+    export default ({
+        props:['name']
+    })
+</script>
