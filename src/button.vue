ce6efbe6ce9f649948c89c9999217dfa059f27a4
zkjaisq <18771964684@163.com>
2019-03-06 11:12:55 +0800

    修改props传值并进行验证

--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,32 @@
 <template>
-    <button class="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href=`#${icon}`></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </button>
 </template>
 <script>
-    export default({
-        name:'',
+    export default ({
+        name: '',
+//        props: ['icon', 'iconPosition'],//props的传值，父组件中驼峰必须使用"-"，
+        props:{
+            icon:{},
+            iconPosition:{
+                type:String,
+                default:'left',
+//                props自定义验证
+                validator(value){
+                    return value === 'left' || value === 'right';
+                }
+            },
+        }
     })
 </script>
-<style  lang="scss" >
-    .g-button{
+<style lang="scss">
+    .g-button {
         outline: none;
         font-size: var(--font-size);
         height: var(--button-height);
@@ -16,18 +35,36 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background-color: var(--button-bg);
-       &:hover{
-            border: 1px solid var(--border-color-hover);
+        vertical-align: middle;
+        padding: 0 .5em;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+
+    &:hover {
+        border: 1px solid var(--border-color-hover);
+    }
+
+    &:active {
+        background-color: var(--button-active-bg);
+    }
+     >.icon{
+         order: 1;
+         margin-right: .5em;
+     }
+    >.content{
+        order: 2;
+    }
+    &.icon-right {
+        >.content{
+            order: 1;
         }
-        &:active{
-            background-color: var(--button-active-bg);
+        >.icon{
+            order: 2;
+            margin-left: .5em;
         }
     }
 
-    #app{
-        margin: 20px;
     }
-    body{
-        font-size: var(--font-size);
-    }
+
 </style>
