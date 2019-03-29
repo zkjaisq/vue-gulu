e2309544ec82daebfdf86352a88badef35f03bbb
zkjaisq <18771964684@163.com>
2019-03-29 11:47:56 +0800

    提升版本，解决node不支持import语法的问题

---
+++ index.js
@@ -0,0 +1,7 @@
+import Button from './src/button'
+import ButtonGroup from  './src/button-group'
+import Icon from './src/icon'
+
+export {ButtonGroup,Button,Icon }
+
+//node 现在不支持import，所以不能使用import引入
